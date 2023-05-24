aa6e6c8d10c1dddef75eaff1fe8ffba6582596f3
Paul Sanders <psanders1@gmail.com>
2023-05-24 15:06:50 -0400

    Add to excel

--- sas7bdat_converter_cli/main.py
+++ sas7bdat_converter_cli/main.py
@@ -2,7 +2,9 @@ from pathlib import Path
 from typing import Union
 
 from sas7bdat_converter import dir_to_csv as converter_dir_to_csv
+from sas7bdat_converter import dir_to_excel as converter_dir_to_excel
 from sas7bdat_converter import to_csv as converter_to_csv
+from sas7bdat_converter import to_excel as converter_to_excel
 from typer import Argument, Exit, Option, Typer, echo
 
 __version__ = "0.1.0"
@@ -15,6 +17,7 @@ def to_csv(
     file_path: Path = Argument(..., help="Path to the file to convert", show_default=False),
     export_file: Path = Argument(..., help="Path to the new csv file", show_default=False),
 ) -> None:
+    """Convert a sas7bdat or xpt file to a csv file."""
     if file_path.suffix != ".sas7bdat" and file_path.suffix != ".xpt":
         exit("File must be either a sas7bdat file or an xpt file")
 
@@ -33,12 +36,46 @@ def dir_to_csv(
         None,
         "--output-dir",
         "-o",
-        help="Path to the directory to save the output files",
+        help="Path to the directory to save the output files. Default = The same directory as dir",
         show_default=False,
     ),
 ) -> None:
+    """Convert a directory containing sas7bdat or xpt files to csv files."""
     export_path = output_dir or dir
-    converter_dir_to_csv(dir_path=dir, export_path=export_path, continue_on_error=True)
+    converter_dir_to_csv(dir_path=dir, export_path=export_path)
+
+
+@app.command()
+def to_excel(
+    file_path: Path = Argument(..., help="Path to the file to convert", show_default=False),
+    export_file: Path = Argument(..., help="Path to the new Excel file", show_default=False),
+) -> None:
+    """Convert a sas7bdat or xpt file to an xlsx file."""
+    if file_path.suffix != ".sas7bdat" and file_path.suffix != ".xpt":
+        exit("File must be either a sas7bdat file or an xpt file")
+
+    if export_file.suffix != ".xlsx":
+        exit("The export file must be a xlsx file")
+
+    converter_to_excel(sas7bdat_file=file_path, export_file=export_file)
+
+
+@app.command()
+def dir_to_excel(
+    dir: Path = Argument(
+        ..., help="Path to the directory to convert", exists=True, show_default=False
+    ),
+    output_dir: Union[Path, None] = Option(
+        None,
+        "--output-dir",
+        "-o",
+        help="Path to the directory to save the output files. Default = The same directory as dir",
+        show_default=False,
+    ),
+) -> None:
+    """Convert a directory of sas7bdat or xpt files to xlsx files."""
+    export_path = output_dir or dir
+    converter_dir_to_excel(dir_path=dir, export_path=export_path)
 
 
 @app.callback(invoke_without_command=True)
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,6 +1,7 @@
 import shutil
 from pathlib import Path
 
+import pandas as pd
 import pytest
 
 from sas7bdat_converter_cli.main import __version__, app
@@ -128,5 +129,107 @@ def test_dir_to_csv_same_dir_path_xpt(xpt_dir, test_runner, tmp_path):
     test_runner.invoke(app, args, catch_exceptions=False)
     sas_counter = len([name for name in tmp_path.iterdir() if name.suffix == ".xpt"])
     convert_counter = len([name for name in tmp_path.iterdir() if name.suffix == ".csv"])
+
+    assert sas_counter == convert_counter
+
+
+@pytest.mark.parametrize(
+    "fixture_name, expected_name",
+    [("sas_file_1", "file1.xlsx"), ("sas_file_2", "file2.xlsx"), ("sas_file_3", "file3.xlsx")],
+)
+def test_to_excel_sas(fixture_name, expected_name, expected_dir, test_runner, tmp_path, request):
+    sas_file = Path(request.getfixturevalue(fixture_name))
+    converted_file = tmp_path / expected_name
+    expected_file = expected_dir / expected_name
+    args = ["to-excel", str(sas_file), str(converted_file)]
+    test_runner.invoke(app, args, catch_exceptions=False)
+
+    df_expected = pd.read_excel(expected_file, engine="openpyxl")
+    df_converted = pd.read_excel(converted_file, engine="openpyxl")
+
+    pd.testing.assert_frame_equal(df_expected, df_converted)
+
+
+@pytest.mark.parametrize(
+    "fixture_name, expected_name",
+    [("xpt_file_1", "file1.xlsx"), ("xpt_file_2", "file2.xlsx")],
+)
+def test_to_excel_xpt(
+    fixture_name, expected_name, xpt_expected_dir, test_runner, tmp_path, request
+):
+    xpt_file = Path(request.getfixturevalue(fixture_name))
+    converted_file = tmp_path / expected_name
+    expected_file = xpt_expected_dir / expected_name
+    args = ["to-excel", str(xpt_file), str(converted_file)]
+    test_runner.invoke(app, args, catch_exceptions=False)
+
+    df_expected = pd.read_excel(expected_file, engine="openpyxl")
+    df_converted = pd.read_excel(converted_file, engine="openpyxl")
+
+    pd.testing.assert_frame_equal(df_expected, df_converted)
+
+
+def test_to_excel_invalid_extension(test_runner, tmp_path):
+    bad = tmp_path / "bad.txt"
+    converted_file = tmp_path / "test.xlsx"
+    args = ["to-excel", str(bad), str(converted_file)]
+    result = test_runner.invoke(app, args, catch_exceptions=False)
+    out = result.stdout
+
+    assert "File must be either a sas7bdat file or an xpt file" in out
+
+
+def test_to_excel_invalid_output_extension(test_runner, tmp_path):
+    sas_file = tmp_path / "file.sas7bdat"
+    converted_file = tmp_path / "test.txt"
+    args = ["to-excel", str(sas_file), str(converted_file)]
+    result = test_runner.invoke(app, args, catch_exceptions=False)
+    out = result.stdout
+
+    assert "The export file must be a xlsx file" in out
+
+
+@pytest.mark.parametrize("flag", ["--output-dir", "-o"])
+def test_dir_to_excel_different_dir_sas(flag, sas7bdat_dir, test_runner, tmp_path):
+    args = ["dir-to-excel", str(sas7bdat_dir), flag, str(tmp_path)]
+    test_runner.invoke(app, args, catch_exceptions=False)
+    sas_counter = len([name for name in sas7bdat_dir.iterdir() if name.suffix == ".sas7bdat"])
+    convert_counter = len([name for name in tmp_path.iterdir() if name.suffix == ".xlsx"])
+
+    assert sas_counter == convert_counter
+
+
+@pytest.mark.parametrize("flag", ["--output-dir", "-o"])
+def test_dir_to_excel_different_dir_xpt(flag, xpt_dir, test_runner, tmp_path):
+    args = ["dir-to-excel", str(xpt_dir), flag, str(tmp_path)]
+    test_runner.invoke(app, args, catch_exceptions=False)
+    sas_counter = len([name for name in xpt_dir.iterdir() if name.suffix == ".xpt"])
+    convert_counter = len([name for name in tmp_path.iterdir() if name.suffix == ".xlsx"])
+
+    assert sas_counter == convert_counter
+
+
+def test_dir_to_excel_same_dir_path_sas(sas7bdat_dir, test_runner, tmp_path):
+    sas_files = [str(x) for x in sas7bdat_dir.iterdir()]
+    for sas_file in sas_files:
+        shutil.copy(sas_file, str(tmp_path))
+
+    args = ["dir-to-excel", str(tmp_path)]
+    test_runner.invoke(app, args, catch_exceptions=False)
+    sas_counter = len([name for name in tmp_path.iterdir() if name.suffix == ".sas7bdat"])
+    convert_counter = len([name for name in tmp_path.iterdir() if name.suffix == ".xlsx"])
+
+    assert sas_counter == convert_counter
+
+
+def test_dir_to_excel_same_dir_path_xpt(xpt_dir, test_runner, tmp_path):
+    sas_files = [str(x) for x in xpt_dir.iterdir()]
+    for sas_file in sas_files:
+        shutil.copy(sas_file, str(tmp_path))
+
+    args = ["dir-to-excel", str(tmp_path)]
+    test_runner.invoke(app, args, catch_exceptions=False)
+    sas_counter = len([name for name in tmp_path.iterdir() if name.suffix == ".xpt"])
+    convert_counter = len([name for name in tmp_path.iterdir() if name.suffix == ".xlsx"])
 
     assert sas_counter == convert_counter
